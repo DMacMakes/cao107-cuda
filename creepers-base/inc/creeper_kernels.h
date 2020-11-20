625405e308828554f3a15054c00564f1ccc498bb
DESKTOP-HOME\Danny <danny.mcgillick@gmail.com>
2020-11-20 12:39:41 +1100

    A little more cleaning, getting rid of useful but confusing inc folder contents.

--- creepers-base/inc/creeper_kernels.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <cuda_runtime.h>
-
-// Take two arrays of ints, add corresponding addends and place sum in.. sums array.
-__global__ void sumIntArrays_k(const int* addends_1, const int* addends_2, int* sums);
-
